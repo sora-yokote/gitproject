cd377c801b8e0b6a23670aa9d09ce49583f76e40
student <student@FJ-1025>
2026-05-15 12:12:38 +0900

    Javaファイル登録

---
+++ src/gitproject/test.java
@@ -0,0 +1,17 @@
+package gitproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		int number = 10;
+		for (int i = 1; i <= number; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i + ":奇数");
+			} else if (i % 2 == 0) {
+				System.out.println(i + ":偶数");
+			}
+
+		}
+
+	}
+}
